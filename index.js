2dad5abf178322b11bd259140c109607617e7a2e
Yumegaarux <zaldarriagajanvictor@gmail.com>
2024-06-04 22:46:04 +0800

    few more visual changes

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-window.addEventListener('scroll', function(){
-    var navbar = document.getElementById('navbar');
-    if (window.scrollY > ) { // You can adjust the value based on when you want the effect to happen
-        navbar.classList.add('navbar-scrolled');
-        navbar.classList.remove('navbar-normal');
-    } else {
-        navbar.classList.remove('navbar-scrolled');
-        navbar.classList.add('navbar-normal');
-    }
+window.addEventListener("scroll", () => {
+    const main = document.getElementById("main");
+    const mainTop = main.getBoundingClientRect().top;
+
 });
